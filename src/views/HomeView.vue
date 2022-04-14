9ab4f56d4487e481372b4b96e049aa600363a8a4
msplint <tk@terrifickid.net>
2022-04-13 20:25:45 -0500

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,9 +12,9 @@
         The art world's DAO.<br />Collecting contemporary art together.
       </p>
       <a
-        href="#"
+        href="http://app.salondao.xyz:3000"
         class="border-2 p-5 bg-black text-white text-2xl rounded-full border-black"
-        >Enter dapp</a
+        >Enter App</a
       >
     </div>
   </div>
