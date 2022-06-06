32e064c21b8785537da4872a6a0018bf97ca2f12
msplint <tk@terrifickid.net>
2022-06-06 14:24:47 -0500

    autodeploy

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@
     <div class="grid grid-cols-12">
       <div class="col-span-4">
         <ul>
-          <li class="mb-2"><a href="/about">About Salon</a></li>
-          <li class="mb-2"><a href="/collection">Collection</a></li>
-          <li class="mb-2"><a href="/members">Members</a></li>
+          <li class="mb-2"><a href="/#/about">About Salon</a></li>
+          <li class="mb-2"><a href="/#/collection">Collection</a></li>
+          <li class="mb-2"><a href="/#/members">Members</a></li>
         </ul>
       </div>
       <div class="col-span-8"><router-view /></div>
