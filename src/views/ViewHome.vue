a3bf11c82839f7f16dfb993843bf5e559b40c205
msplint <tk@terrifickid.net>
2022-06-23 05:42:51 -0500

    autodeploy

--- src/views/ViewHome.vue
+++ src/views/ViewHome.vue
@@ -11,22 +11,24 @@
         </div>
       </div>
       <div class="bottom-0 right-0 absolute p-3">
-        <AppButton href="/#/join" :colors="colors" class="flex items-center"
-          ><svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5 mr-1"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="2"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M13 7l5 5m0 0l-5 5m5-5H6"
-            />
-          </svg>
-          Join</AppButton
+        <router-link to="/join">
+          <AppButton :colors="colors" class="flex items-center"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5 mr-1"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M13 7l5 5m0 0l-5 5m5-5H6"
+              />
+            </svg>
+            Join</AppButton
+          ></router-link
         >
       </div>
     </div>
