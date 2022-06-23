b5c0e1187718131b5ece8b3049479174dd56824d
msplint <tk@terrifickid.net>
2022-06-23 02:37:55 -0500

    autodeploy

--- src/components/Form/InputUpload.vue
+++ src/components/Form/InputUpload.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>
+  <div class="">
     <FormLabel :count="count" :required="required">{{ title }}</FormLabel>
-    <input v-show="!progress" type="file" ref="file" @change="execute" />
+    <input
+      v-show="!progress"
+      type="file"
+      ref="file"
+      @change="execute"
+      class="ml-10 sm:ml-0"
+    />
     <div
       v-show="progress"
-      class="bg-black h-4"
+      class="bg-black h-4 ml-10 sm:ml-0"
       :style="{ width: progress + '%' }"
     ></div>
-    <span v-show="progress != 0">
+    <span v-show="progress != 0" class="ml-10 sm:ml-0">
       {{ progress }}%
       <span v-show="progress != 100">Uploading...</span>
       <span v-show="progress == 100">Complete!</span></span
--- src/views/ViewHome.vue
+++ src/views/ViewHome.vue
@@ -7,7 +7,23 @@
         </div>
       </div>
       <div class="bottom-0 right-0 absolute p-3">
-        <AppButton href="/#/join" :colors="colors">Join</AppButton>
+        <AppButton href="/#/join" :colors="colors" class="flex item-center"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6 mr-2"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M17 8l4 4m0 0l-4 4m4-4H3"
+            />
+          </svg>
+          Join</AppButton
+        >
       </div>
     </div>
   </div>
