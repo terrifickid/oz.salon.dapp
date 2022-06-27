3a727ede6b06061d82670a24f4be2afb2592f306
msplint <tk@terrifickid.net>
2022-06-27 13:45:50 -0500

    autodeploy

--- src/components/AppFullpage.vue
+++ src/components/AppFullpage.vue
@@ -24,6 +24,7 @@ export default {
     async resize() {
       console.log("resize!");
       this.$refs.fullpage.api.fitToSection();
+      this.$refs.fullpage.api.reBuild();
     },
   },
   mounted() {
