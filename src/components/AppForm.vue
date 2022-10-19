1a235120dcffb70343cb3da4a7087f0e40e57dbf
msplint <tk@terrifickid.net>
2022-10-19 18:16:25 -0500

    autodeploy

--- src/components/AppForm.vue
+++ src/components/AppForm.vue
@@ -20,6 +20,7 @@
             <span class="flex items-center">
               {{ field.name }}
               <svg
+                v-if="fields.length - 1 != index"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
