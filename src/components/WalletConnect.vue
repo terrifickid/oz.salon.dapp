6e34f7e0a0471feac697ec1de049c79439b16bfa
msplint <tk@terrifickid.net>
2025-03-14 07:04:28 -0500

    update

--- src/components/WalletConnect.vue
+++ src/components/WalletConnect.vue
@@ -22,10 +22,11 @@
         </div>
         <p>
           <AppButton class="mt-8 sm:mt-4 mx-auto"
-            ><span v-if="!isConnecting">Login</span>
+            ><span v-show="!isConnecting">Login</span>
 
             <svg
-              class="size-5 animate-spin"
+              v-show="isConnecting"
+              class="w-5 h-5 animate-spin"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
