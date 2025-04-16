0b9f8f7e15e5a5bb0f25b2312e42167df9604c80
msplint <tk@terrifickid.net>
2025-04-15 22:50:34 -0500

    yes/no fix 1

--- src/components/AppButtonVote.vue
+++ src/components/AppButtonVote.vue
@@ -8,7 +8,10 @@
   >
     <span v-if="!isProcessing">{{ label }}</span>
     <svg
-      class="size-5 animate-spin"
+      v-else
+      class="animate-spin"
+      width="20"
+      height="20"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
@@ -18,12 +21,12 @@
         cx="12"
         cy="12"
         r="10"
-        stroke="currentColor"
+        stroke="black"
         stroke-width="4"
       ></circle>
       <path
         class="opacity-75"
-        fill="currentColor"
+        fill="black"
         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
       ></path>
     </svg>
