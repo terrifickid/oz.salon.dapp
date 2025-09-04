d5c29427ab793131144ac54be40ef3ccd733b941
Richard Croft <tk@terrifickid.net>
2025-09-04 11:44:53 +0000

    form in

--- src/views/ViewContact.vue
+++ src/views/ViewContact.vue
@@ -8,13 +8,85 @@
         <p class="mb-8" v-html="data.fields.title"></p>
         <div v-html="data.fields.code"></div>
         <div v-html="data.fields.code2"></div>
+        <div id="contactForm">
+          <form
+            action="https://fund.us3.list-manage.com/subscribe/post?u=ba60d72b882bcc8cbf54db6b6&amp;id=dcce906d59&amp;f_id=0088dae0f0"
+            method="post"
+            id="mc-embedded-subscribe-form"
+            name="mc-embedded-subscribe-form"
+            class="validate"
+            target="_blank"
+          >
+            <div id="mc_embed_signup_scroll">
+              <div class="mc-field-group mb-4">
+                <label for="mce-EMAIL" class="block mb-1">Email Address </label>
+                <input
+                  type="email"
+                  name="EMAIL"
+                  class="required email border border-black p-2 rounded"
+                  id="mce-EMAIL"
+                  value=""
+                  required
+                />
+              </div>
+              <div class="mc-field-group mb-4">
+                <label for="mce-FNAME" class="block mb-1">First Name </label>
+                <input
+                  type="text"
+                  name="FNAME"
+                  class="text border border-black rounded p-2"
+                  id="mce-FNAME"
+                  value=""
+                />
+              </div>
+              <div class="mc-field-group mb-4">
+                <label for="mce-LNAME" class="block mb-1">Last Name </label>
+                <input
+                  type="text"
+                  name="LNAME"
+                  class="text border border-black rounded p-2"
+                  id="mce-LNAME"
+                  value=""
+                />
+              </div>
+              <div id="mce-responses" class="clear">
+                <div
+                  class="response"
+                  id="mce-error-response"
+                  style="display: none"
+                ></div>
+                <div
+                  class="response"
+                  id="mce-success-response"
+                  style="display: none"
+                ></div>
+              </div>
+              <div aria-hidden="true" style="position: absolute; left: -5000px">
+                <input
+                  type="text"
+                  name="b_ba60d72b882bcc8cbf54db6b6_dcce906d59"
+                  tabindex="-1"
+                  value=""
+                />
+              </div>
+              <div class="clear">
+                <input
+                  type="submit"
+                  name="subscribe"
+                  id="mc-embedded-subscribe"
+                  class="button border border-black p-2 cursor-pointer"
+                  value="Subscribe"
+                />
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
     <div class="lg:col-span-4"></div>
   </AppShell>
 </template>
 <script>
-// @ is an alias to /src
 import axios from "axios";
 import AppShell from "@/components/AppShell";
 
