386ee2040a2698cf2083999ff0df3e7992f8cc6c
Richard Croft <tk@terrifickid.net>
2025-10-15 06:38:42 +0000

    form design update

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="contactForm">
+    <p class="mb-2">Newsletter</p>
+    <form
+      action="https://fund.us3.list-manage.com/subscribe/post?u=ba60d72b882bcc8cbf54db6b6&amp;id=dcce906d59&amp;f_id=0088dae0f0"
+      method="post"
+      id="mc-embedded-subscribe-form"
+      name="mc-embedded-subscribe-form"
+      class="validate"
+      target="_blank"
+    >
+      <div id="mc_embed_signup_scroll">
+        <div class="mc-field-group mb-1">
+          <input
+            type="email"
+            name="EMAIL"
+            class="font-haffer sm:ml-0 block text-black outline-none placeholder-opb bg-gray-100"
+            id="mce-EMAIL"
+            value=""
+            required
+            placeholder="Email Address"
+          />
+        </div>
+        <div class="mc-field-group mb-1">
+          <input
+            type="text"
+            name="FNAME"
+            class="font-haffer sm:ml-0 block text-black outline-none placeholder-opb bg-gray-100"
+            id="mce-FNAME"
+            value=""
+            placeholder="First Name"
+          />
+        </div>
+        <div class="mc-field-group mb-1">
+          <input
+            type="text"
+            name="LNAME"
+            class="font-haffer sm:ml-0 block text-black outline-none placeholder-opb bg-gray-100"
+            id="mce-LNAME"
+            value=""
+            placeholder="Last Name"
+          />
+        </div>
+        <div id="mce-responses" class="clear">
+          <div
+            class="response"
+            id="mce-error-response"
+            style="display: none"
+          ></div>
+          <div
+            class="response"
+            id="mce-success-response"
+            style="display: none"
+          ></div>
+        </div>
+        <div aria-hidden="true" style="position: absolute; left: -5000px">
+          <input
+            type="text"
+            name="b_ba60d72b882bcc8cbf54db6b6_dcce906d59"
+            tabindex="-1"
+            value=""
+          />
+        </div>
+        <div class="clear mt-4">
+          <input
+            type="submit"
+            name="subscribe"
+            id="mc-embedded-subscribe"
+            class="px-3 py-1 rounded-full bg-gray-200 hover:bg-gray-300 block mb-2 cursor-pointer text-base"
+            value="Sign Up"
+          />
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
--- src/views/ViewContact.vue
+++ src/views/ViewContact.vue
@@ -4,109 +4,27 @@
       v-if="loaded"
       class="grid grid-cols-12 pt-20 w-full font-haffer px-5 pb-48 gap-4"
     >
-      <div class="col-span-12 md:col-span-6 text-2xl">
-        <p class="mb-8" v-html="data.fields.title"></p>
-        <div v-html="data.fields.code"></div>
-        <div v-html="data.fields.code2"></div>
+      <div class="col-span-12 md:col-span-6 text-2xl grid grid-cols-12">
+        <p class="mb-8 col-span-12" v-html="data.fields.title"></p>
+        <div class="col-span-12" v-html="data.fields.code"></div>
+        <div class="col-span-6" v-html="data.fields.code2"></div>
+        <div class="col-span-6">
+          <div v-html="data.fields.code3"></div>
+          <ContactForm />
+        </div>
       </div>
 
       <!-- Change col-start-3 to col-start-9 to move form to the right -->
-      <div class="col-span-12 md:col-span-4 md:col-start-9">
-        <div id="contactForm" class="w-full bg-gray-100 p-5">
-          <form
-            action="https://fund.us3.list-manage.com/subscribe/post?u=ba60d72b882bcc8cbf54db6b6&amp;id=dcce906d59&amp;f_id=0088dae0f0"
-            method="post"
-            id="mc-embedded-subscribe-form"
-            name="mc-embedded-subscribe-form"
-            class="validate"
-            target="_blank"
-          >
-            <div id="mc_embed_signup_scroll">
-              <div class="mc-field-group mb-4">
-                <label
-                  for="mce-EMAIL"
-                  class="block mb-10 flex items-center font-haffer"
-                  ><span class="mr-12">1</span>Email Address
-                </label>
-                <input
-                  type="email"
-                  name="EMAIL"
-                  class="font-haffer sm:ml-0 bg-transparent block border-b border-black w-full text-black py-3 outline-none placeholder-opb"
-                  id="mce-EMAIL"
-                  value=""
-                  required
-                />
-              </div>
-              <div class="mc-field-group mb-4">
-                <label
-                  for="mce-FNAME"
-                  class="block mb-10 flex items-center font-haffer"
-                  ><span class="mr-12">2</span>First Name
-                </label>
-                <input
-                  type="text"
-                  name="FNAME"
-                  class="font-haffer sm:ml-0 bg-transparent block border-b border-black w-full text-black py-3 outline-none placeholder-opb"
-                  id="mce-FNAME"
-                  value=""
-                />
-              </div>
-              <div class="mc-field-group mb-4">
-                <label
-                  for="mce-LNAME"
-                  class="block mb-10 flex items-center font-haffer"
-                  ><span class="mr-12">3</span>Last Name
-                </label>
-                <input
-                  type="text"
-                  name="LNAME"
-                  class="font-haffer sm:ml-0 bg-transparent block border-b border-black w-full text-black py-3 outline-none placeholder-opb"
-                  id="mce-LNAME"
-                  value=""
-                />
-              </div>
-              <div id="mce-responses" class="clear">
-                <div
-                  class="response"
-                  id="mce-error-response"
-                  style="display: none"
-                ></div>
-                <div
-                  class="response"
-                  id="mce-success-response"
-                  style="display: none"
-                ></div>
-              </div>
-              <div aria-hidden="true" style="position: absolute; left: -5000px">
-                <input
-                  type="text"
-                  name="b_ba60d72b882bcc8cbf54db6b6_dcce906d59"
-                  tabindex="-1"
-                  value=""
-                />
-              </div>
-              <div class="clear">
-                <input
-                  type="submit"
-                  name="subscribe"
-                  id="mc-embedded-subscribe"
-                  class="px-4 py-2 rounded-full bg-gray-200 hover:bg-gray-300 block mb-2 cursor-pointer"
-                  value="Submit"
-                />
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
+      <div class="col-span-12 md:col-span-4 md:col-start-9"></div>
     </div>
   </AppShell>
 </template>
 <script>
 import axios from "axios";
 import AppShell from "@/components/AppShell";
-
+import ContactForm from "@/components/ContactForm";
 export default {
-  components: { AppShell },
+  components: { AppShell, ContactForm },
   data() {
     return {
       data: {},
